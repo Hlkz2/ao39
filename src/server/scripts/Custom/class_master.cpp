5072127a8a0d1a005d2d517893c58e80c1671db6
Hlkz <hlkz@hotmail.fr>
2012-07-16 20:43:02 +0200

    double spe + ench/job/tp

--- src/server/scripts/Custom/class_master.cpp
+++ src/server/scripts/Custom/class_master.cpp
@@ -1,94 +1,81 @@
 #include "ScriptPCH.h"
 
+class class_master : public CreatureScript {
+    public: class_master() : CreatureScript("class_master") {}
 
-class class_master : public CreatureScript
-{
-    public:
-        class_master() : CreatureScript("class_master") { }
-
-        bool OnGossipHello(Player* player, Creature* creature)
-        { MainMenu(player, creature);
-		return true; }
+bool OnGossipHello(Player* player, Creature* creature) {
+	MainMenu(player, creature);
+	return true; }
 		
-		void MainMenu(Player *player, Creature *creature)
-		{
-			player->ADD_GOSSIP_ITEM( 3, "Mise a niveau des sorts, stats et compétences" , GOSSIP_SENDER_MAIN, 1);
-			player->ADD_GOSSIP_ITEM( 2, "Apprentissage de la double spécalisation" , GOSSIP_SENDER_MAIN, 2);
-			player->ADD_GOSSIP_ITEM( 1, "Vendeur de glyphes." , GOSSIP_SENDER_MAIN, 3);
-			player->ADD_GOSSIP_ITEM( 2, "Réinitialisation des talents" , GOSSIP_SENDER_MAIN, 4);
-			player->SEND_GOSSIP_MENU(1000003, creature->GetGUID());
+void MainMenu(Player *player, Creature *creature) {
+	player->ADD_GOSSIP_ITEM( 3, "Mise a niveau des sorts, stats et compétences" , GOSSIP_SENDER_MAIN, 1);
+	player->ADD_GOSSIP_ITEM( 2, "Apprentissage de la double spécalisation" , GOSSIP_SENDER_MAIN, 2);
+	player->ADD_GOSSIP_ITEM( 1, "Vendeur de glyphes." , GOSSIP_SENDER_MAIN, 3);
+	player->ADD_GOSSIP_ITEM( 2, "Réinitialisation des talents" , GOSSIP_SENDER_MAIN, 4);
+	player->SEND_GOSSIP_MENU(1000003, creature->GetGUID()); }
+
+bool OnGossipSelect(Player* player, Creature* creature, uint32 /*sender*/, uint32 action) {
+
+		switch (action) {
+
+	case 1:
+		MainMenu(player, creature);
+		player->UpdateSkillsToMaxSkillsForLevel();
+		if (player->HasSpell(17877)) {
+		player->learnSpell(18867, true);
+		player->learnSpell(18868, true); } // brulure de l'ombre
+		if (player->HasSpell(11366)) {
+		player->learnSpell(12505, true);
+		player->learnSpell(12522, true);
+		player->learnSpell(12523, true); } // explosion pyrotechnique
+		if (player->HasSpell(11113)) {
+		player->learnSpell(13018, true); } // vague explosive
+		if (player->HasSpell(5570)) {
+		player->learnSpell(24974, true); } // essaim d'insecte
+		if (player->HasSpell(19434)) {
+		player->learnSpell(20900, true);
+		player->learnSpell(20901, true); } // visée
+		if (player->HasSpell(15407)) {
+		player->learnSpell(17311, true);
+		player->learnSpell(17312, true); } // fouet mental
+		if (player->HasSpell(19236)) {
+		player->learnSpell(19240, true);
+		player->learnSpell(19238, true); } // prière du désespoir
+		creature->CastSpell(player,38588,false);
+		break;
+
+	case 2:
+		MainMenu(player, creature);
+		if (player->GetSpecsCount() == 1) {
+			player->CastSpell(player, 63680, true, NULL, NULL, player->GetGUID());
+			player->CastSpell(player, 63624, true, NULL, NULL, player->GetGUID()); }
+		break;
+	
+	case 3:
+		player->GetSession()->SendListInventory(creature->GetGUID());
+		break;
+
+	case 30:
+		if(player->getClass() == 1){    player->GetSession()->SendListInventory(999991); } // guerrier
+		if(player->getClass() == 2){	player->GetSession()->SendListInventory(999992); } // paladin
+		if(player->getClass() == 3){	player->GetSession()->SendListInventory(999993); } // chasseur
+		if(player->getClass() == 4){	player->GetSession()->SendListInventory(999994); } // voleur
+		if(player->getClass() == 5){	player->GetSession()->SendListInventory(999995); } // prêtre
+		if(player->getClass() == 7){	player->GetSession()->SendListInventory(999996); } // chaman
+		if(player->getClass() == 8){	player->GetSession()->SendListInventory(999997); } // mage
+		if(player->getClass() == 9){	player->GetSession()->SendListInventory(999998); } // démoniste
+		if(player->getClass() == 11){	player->GetSession()->SendListInventory(999999); } // druide
+		break;
+	
+	case 4:
+		MainMenu(player, creature);
+		player->resetTalents(true);
+ 		player->SendTalentsInfoData(false);
+		break;
+	
 		}
-
-        bool OnGossipSelect(Player* player, Creature* creature, uint32 /*sender*/, uint32 action)
-        {
-            if (player->isInCombat())
-            {
-                player->CLOSE_GOSSIP_MENU();
-                creature->MonsterWhisper("Vous etes en combat !", player->GetGUID(), false);
-                return false;
-            }
-		
-				switch (action)
-	            {
-					case 1:
-						player->CLOSE_GOSSIP_MENU();
-						player->UpdateSkillsToMaxSkillsForLevel();
-						if (player->HasSpell(17877)) {
-						player->learnSpell(18867, true);
-						player->learnSpell(18868, true); } // brulure de l'ombre
-						if (player->HasSpell(11366)) {
-						player->learnSpell(12505, true);
-						player->learnSpell(12522, true);
-						player->learnSpell(12523, true); } // explosion pyrotechnique
-						if (player->HasSpell(11113)) {
-						player->learnSpell(13018, true); } // vague explosive
-						if (player->HasSpell(5570)) {
-						player->learnSpell(24974, true); } // essaim d'insecte
-						if (player->HasSpell(19434)) {
-						player->learnSpell(20900, true);
-						player->learnSpell(20901, true); } // visée
-						if (player->HasSpell(15407)) {
-						player->learnSpell(17311, true);
-						player->learnSpell(17312, true); } // fouet mental
-						if (player->HasSpell(19236)) {
-						player->learnSpell(19240, true);
-						player->learnSpell(19238, true); } // prière du désespoir
-						creature->CastSpell(player,38588,false);
-					break;
-
-					case 2:
-						player->CLOSE_GOSSIP_MENU();
-						player->CastSpell(player, 63680, true);
-						player->CastSpell(player, 63624, true);
-					break;
-					
-					case 3:
-						player->GetSession()->SendListInventory(creature->GetGUID());
-					break;
-
-	                case 30:
-						if(player->getClass() == 1){    player->GetSession()->SendListInventory(999991); } // guerrier
-						if(player->getClass() == 2){	player->GetSession()->SendListInventory(999992); } // paladin
-						if(player->getClass() == 3){	player->GetSession()->SendListInventory(999993); } // chasseur
-						if(player->getClass() == 4){	player->GetSession()->SendListInventory(999994); } // voleur
-						if(player->getClass() == 5){	player->GetSession()->SendListInventory(999995); } // prêtre
-						if(player->getClass() == 7){	player->GetSession()->SendListInventory(999996); } // chaman
-						if(player->getClass() == 8){	player->GetSession()->SendListInventory(999997); } // mage
-						if(player->getClass() == 9){	player->GetSession()->SendListInventory(999998); } // démoniste
-						if(player->getClass() == 11){	player->GetSession()->SendListInventory(999999); } // druide
-                    break;
-					
-					case 4:
-						player->CLOSE_GOSSIP_MENU();
-						player->resetTalents(true);
- 						player->SendTalentsInfoData(false);
-					break;
-
-	            }
-        }
+    return true; }
 };
 
-void AddSc_CustomClass()
-{
-    new class_master;
-}
+void AddSc_CustomClass() {
+    new class_master; }
--- src/server/scripts/Custom/universal_teleporter.cpp
+++ src/server/scripts/Custom/universal_teleporter.cpp
@@ -1,104 +1,84 @@
 #include "ScriptPCH.h"
 #include "Config.h"
 
-class universal_teleporter : public CreatureScript
-{
-public:
-	universal_teleporter()
-		: CreatureScript("universal_teleporter")
-	{}
+class universal_teleporter : public CreatureScript {
+	public: universal_teleporter() : CreatureScript("universal_teleporter") {}
 
-	void MainMenu(Player *pPlayer, Creature *pCreature)
-	{
-		std::ostringstream ostr;
-		ostr << "[FFA] " << ConfigMgr::GetStringDefault("u_t_s", "");
+void MainMenu(Player *player, Creature *creature) {
 
-		switch (pPlayer->GetTeam()) {
-			case ALLIANCE:
-				pPlayer->ADD_GOSSIP_ITEM(6, "[Shop] Karazhan", GOSSIP_SENDER_MAIN, 10);
-				pPlayer->ADD_GOSSIP_ITEM(9, "[PvP sauvage] Cratère d'Azshara", GOSSIP_SENDER_MAIN, 12);
-			break;
-			case HORDE:
-				pPlayer->ADD_GOSSIP_ITEM(6, "[Shop] Karazhan", GOSSIP_SENDER_MAIN, 11);
-				pPlayer->ADD_GOSSIP_ITEM(9, "[PvP sauvage] Cratère d'Azshara", GOSSIP_SENDER_MAIN, 13);
-			break; }
-				pPlayer->ADD_GOSSIP_ITEM(9, "[Duel] Ahn Qiraj", GOSSIP_SENDER_MAIN, 16);
-				pPlayer->ADD_GOSSIP_ITEM(9, ostr.str().c_str(), GOSSIP_SENDER_MAIN, 20);
-				pPlayer->ADD_GOSSIP_ITEM(6, "Dalaran (accès au monde...)", GOSSIP_SENDER_MAIN, 14);
-		pPlayer->SEND_GOSSIP_MENU(1, pCreature->GetGUID());
-	}
+	std::ostringstream ostr;
+	ostr << "[FFA] " << ConfigMgr::GetStringDefault("u_t_s", "");
+	
+	if (player->GetTeam() == ALLIANCE) {
+		player->ADD_GOSSIP_ITEM(6, "[Shop] Karazhan", GOSSIP_SENDER_MAIN, 10);
+		player->ADD_GOSSIP_ITEM(9, "[PvP sauvage] Cratère d'Azshara", GOSSIP_SENDER_MAIN, 12); }
+	if (player->GetTeam() == HORDE) {
+		player->ADD_GOSSIP_ITEM(6, "[Shop] Karazhan", GOSSIP_SENDER_MAIN, 11);
+		player->ADD_GOSSIP_ITEM(9, "[PvP sauvage] Cratère d'Azshara", GOSSIP_SENDER_MAIN, 13); }
+	player->ADD_GOSSIP_ITEM(9, "[Duel] Ahn Qiraj", GOSSIP_SENDER_MAIN, 16);
+	player->ADD_GOSSIP_ITEM(9, ostr.str().c_str(), GOSSIP_SENDER_MAIN, 20);
+	player->ADD_GOSSIP_ITEM(6, "Dalaran (accès au monde...)", GOSSIP_SENDER_MAIN, 14);
+	player->SEND_GOSSIP_MENU(1, creature->GetGUID()); }
+	
+bool OnGossipHello(Player* player, Creature* creature) {
+	MainMenu(player, creature);
+	return true; }
+
+bool OnGossipSelect(Player* player, Creature* creature, uint32 /*uiSender*/, uint32 uiAction) {
+
+	uint32 u_t_m = ConfigMgr::GetIntDefault("u_t_m", NULL);
+	float u_t_x = ConfigMgr::GetFloatDefault("u_t_x", NULL);
+	float u_t_y = ConfigMgr::GetFloatDefault("u_t_y", NULL);
+	float u_t_z = ConfigMgr::GetFloatDefault("u_t_z", NULL);
+	float u_t_o = ConfigMgr::GetFloatDefault("u_t_o", NULL);
+
+	if (player->isInCombat() && player->GetMapId() != 531 && player->GetMapId() != 532) {
+		creature->MonsterWhisper("Vous êtes en combat.", player->GetGUID());
+		player->CLOSE_GOSSIP_MENU();
+		return true; }
+
+	player->PlayerTalkClass->ClearMenus();
+
+		switch (uiAction) {
+
+	case 1: MainMenu(player, creature); break;
+
+	case 10: {
+		player->CLOSE_GOSSIP_MENU();
+		player->TeleportTo(532, -10945.8f, -2103.85f, 92.8f, 0.9f);
+		break; }
+	case 11: {
+		player->CLOSE_GOSSIP_MENU();
+		player->TeleportTo(532, -11233.4f, -1697.76f, 179.3f, 0.6f);
+		break; }
 		
+	case 12: {
+		player->CLOSE_GOSSIP_MENU();
+		player->TeleportTo(37, -120.103f, -52.833f, 268.7f, 0.1f);
+		break; }
 
-	bool OnGossipHello(Player* pPlayer, Creature* pCreature) {
-		MainMenu(pPlayer, pCreature);
-        return true; }
+	case 13: {
+		player->CLOSE_GOSSIP_MENU();
+		player->TeleportTo(37, 362.67f, 97.629f, 247.5f, 3.1f);
+		break; }
 
+	case 14: {
+		player->CLOSE_GOSSIP_MENU();
+		player->TeleportTo(571, 5819.49f, 642.93f, 647.9f, 0.1f);
+		break; }
 
-    bool OnGossipSelect(Player* pPlayer, Creature* pCreature, uint32 /*uiSender*/, uint32 uiAction)
-    {
+	case 16: {
+		player->CLOSE_GOSSIP_MENU();
+		player->TeleportTo(531, -8512.5f, 2016.02f, 104.6f, 3.6f);
+		break; }
+	
+	case 20: {
+		player->CLOSE_GOSSIP_MENU();
+		player->TeleportTo(u_t_m, u_t_x, u_t_y, u_t_z, u_t_o);
+		break; }
 		
-		uint32 u_t_m = ConfigMgr::GetIntDefault("u_t_m", NULL);
-		float u_t_x = ConfigMgr::GetFloatDefault("u_t_x", NULL);
-		float u_t_y = ConfigMgr::GetFloatDefault("u_t_y", NULL);
-		float u_t_z = ConfigMgr::GetFloatDefault("u_t_z", NULL);
-		float u_t_o = ConfigMgr::GetFloatDefault("u_t_o", NULL);
-
-		if (pPlayer->isInCombat())
-		{
-			pCreature->MonsterWhisper("Vous etes en combat.", pPlayer->GetGUID());
-			pPlayer->CLOSE_GOSSIP_MENU();
-			return true;
 		}
-
-		pPlayer->PlayerTalkClass->ClearMenus();
-
-		switch (uiAction)
-		{
-			case 1:
-			MainMenu(pPlayer, pCreature);
-			break;
-			
-			case 10: {
-				pPlayer->CLOSE_GOSSIP_MENU();
-				pPlayer->TeleportTo(532, -10945.8f, -2103.85f, 92.8f, 0.9f);
-				break; }
-
-			case 11: {
-				pPlayer->CLOSE_GOSSIP_MENU();
-				pPlayer->TeleportTo(532, -11233.4f, -1697.76f, 179.3f, 0.6f);
-				break; }
-			
-			case 12: {
-				pPlayer->CLOSE_GOSSIP_MENU();
-				pPlayer->TeleportTo(37, -120.103f, -52.833f, 268.7f, 0.1f);
-				break; }
-			
-			case 13: {
-				pPlayer->CLOSE_GOSSIP_MENU();
-				pPlayer->TeleportTo(37, 362.67f, 97.629f, 247.5f, 3.1f);
-				break; }
-			case 14: {
-				pPlayer->CLOSE_GOSSIP_MENU();
-				pPlayer->TeleportTo(571, 5819.49f, 642.93f, 647.9f, 0.1f);
-				break; }
-			case 15: {
-				pPlayer->CLOSE_GOSSIP_MENU();
-				pPlayer->TeleportTo(0, -4803.11f, -975.12f, 399.1f, 3.3f);
-				break; }
-			case 16: {
-				pPlayer->CLOSE_GOSSIP_MENU();
-				pPlayer->TeleportTo(531, -8512.5f, 2016.02f, 104.6f, 3.6f);
-				break; }
-
-					 
-			case 20: {
-				pPlayer->CLOSE_GOSSIP_MENU();
-				pPlayer->TeleportTo(u_t_m, u_t_x, u_t_y, u_t_z, u_t_o);
-				break; }
-		}
-
-        return true;
-	}
+    return true; }
 };
 
 void AddSc_Script_UTeleporter()
